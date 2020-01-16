1d965817ba4c8188ab48047489d9640a5c2d8c1a
jacky jean louis <j.st.jeanlouis@gmail.com>
2020-01-16 21:53:27 +0100

    fix controllers

--- app/models/freegrapher.rb
+++
@@ -1,3 +0,0 @@
-class Freegrapher < ApplicationRecord
-  belongs_to :user
-end
--- test/controllers/tests_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class TestsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get tests_index_url
-    assert_response :success
-  end
-
-end
